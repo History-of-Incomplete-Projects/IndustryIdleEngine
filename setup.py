64111cbb0e98117b755ff8bd1f4aa7a35440092f
Alan <ahuang.jm@outlook.com>
2021-04-28 15:05:41 -0600

    added setup and for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dbmanager',
+    packages=find_packages(include=['dbmanager']),
+    version='0.1.0',
+    description='A database manager that makes sqlalchemy easier',
+    author='jingming@ualberta.ca',
+    license='Apache',
+    install_requires=[
+        'sqlalchemy',
+        'pillow==8.2.0',
+        'numpy==1.20.2',
+        'pyautogui==0.9.52',
+        'opencv-python==4.5.1.48',
+        'pywin32',
+        'sqlalchemy==1.4.9',
+        'BeautifulSoup4==4.9.3',
+        'PyQT5==5.15.4',
+        'scipy==1.6.2',
+        'dbmanager @ https://github.com/History-of-Incomplete-Projects/DBManager/archive/refs/heads/main.zip',
+    ]
+)
